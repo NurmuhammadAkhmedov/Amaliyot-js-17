e97745c08e8653a53b65a6e74286ccf831cedb3e
NurmuhammadAkhmedov <ahmedov13nurmuhammad@gmail.com>
2025-02-08 15:37:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+let textColor = prompt("Enter text color:", "white");
+let bgColor = prompt("Enter body backgroundColor:", "black");
+
+document.body.style.color = textColor;
+document.body.style.backgroundColor = bgColor;
